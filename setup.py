7d05bc6e20983d6d67018e34540f0f5f8121f2db
root <root@vmi3125379.contaboserver.net>
2026-03-21 07:27:43 +0100

    ci: add setup.py; install package in CI to fix imports

--- setup.py
+++ setup.py
@@ -2,13 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="web3-security-scout",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[],
-    entry_points={
-        "console_scripts": [
-            "w3-scout=web3_security_scout.cli:main",
-        ],
-    },
-    python_requires=">=3.8",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
